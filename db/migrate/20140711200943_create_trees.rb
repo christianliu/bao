d99246cbb6b5c465884e28eac28d3049d7151390
Zack Chauvin <zack@iexperience.co.za>
2014-08-06 00:08:10 +0200

    cleaned up migrations

--- db/migrate/20140711200943_create_trees.rb
+++ db/migrate/20140711200943_create_trees.rb
@@ -1,8 +1,8 @@
 class CreateTrees < ActiveRecord::Migration
   def change
     create_table :trees do |t|
-      t.string :name
       t.integer :user_id
+      t.integer :rand_given
 
       t.timestamps
     end
